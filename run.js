eabc08f677c2a1995a36a8b9cb21ecc979a94c78
XAi <gunturbalantara0412@gmail.com>
2026-05-17 14:21:26 +0700

    Create run.js

---
+++ run.js
@@ -0,0 +1,19 @@
+const TurnstileSolver = require('./');
+
+(async () => {
+  const solver = new TurnstileSolver({
+    record: true,          // aktifkan recording
+    recordDir: './recordings',
+    timeout: 60000,
+  });
+
+  // pakai sitekey eksplisit
+  const res = await solver.solve("https://www.waifu2x.net",
+      "0x4AAAAAABqlY7DKXMzoS81U");
+  console.log(res);
+
+  // atau langsung dari halaman (auto-detect)
+  // const res = await solver.solve('https://target.com');
+
+  await solver.cleanup();
+})();
